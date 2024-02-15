4058dbacca22f796ed74f062669419568b0b24ee
B-AJ-Amar <amarbouakaz91@gmail.com>
2024-02-16 00:04:41 +0100

    ADD: basic setup for django channels

--- src/src/settings.py
+++ src/src/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,6 +68,8 @@ TEMPLATES = [
     },
 ]
 
+
+ASGI_APPLICATION = 'src.asgi.application'
 WSGI_APPLICATION = 'src.wsgi.application'
 
 
